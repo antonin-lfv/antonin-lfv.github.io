a57d960a3afef5c5bbb968ac6176f0638697748f
antonin-lfv <antoninlefevre45@icloud.com>
2026-01-10 11:36:37 +0000

    Deploying to gh-pages from @ antonin-lfv/antonin-lfv.github.io@41eecb0f7f644e8960f8e050eab48df541f8d9ad 🚀

--- assets/js/cronitor-analytics-setup.js
+++ assets/js/cronitor-analytics-setup.js
@@ -3,4 +3,4 @@ window.cronitor =
   function () {
     (window.cronitor.q = window.cronitor.q || []).push(arguments);
   };
-cronitor("config", { clientKey: "" });
+cronitor("config", { clientKey: "d2fc3713697282d515aa5406812bc104" });
